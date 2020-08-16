f9ca59fd96943f227ce0675740270ab3be685e17
Guillermo Granados Gómez <guillermogranadosgomez@gmail.com>
2020-08-16 13:27:45 +0200

    solucionado un fallo en personas

--- taller/taller/settings.py
+++ taller/taller/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '(x*qz-txbw_)zjj@$nj=zy%3$*jw%_bgobig3ab^*)xlgw@s+)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'tallerpython.pythonanywhere.com']
 
